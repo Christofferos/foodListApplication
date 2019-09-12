34984979fcbb6296608d1a5b11d80a99df6ef534
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-09-12 16:27:19 +0200

    Yes

--- client/src/store.js
+++ client/src/store.js
@@ -9,16 +9,23 @@ const middleWare = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // Only chrome can handle the redux dev tool
 // redux compose cannot handle a null or undefined middleware
-const store;
-if (window.navigator.userAgent.includes('Chrome')) {
-   store = createStore(rootReducer, initialState, compose(
- applyMiddleware(...middleware),
- window._REDUX_DEVTOOLS_EXTENSION_ && window.__REDUX_DEVTOOLS_EXTENSION__()
- ));
+var store;
+if (window.navigator.userAgent.includes("Chrome")) {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window._REDUX_DEVTOOLS_EXTENSION_ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
 } else {
-   store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
 }
-
 
 /*
 Tutorial way of doing. Does not work on Safari!
